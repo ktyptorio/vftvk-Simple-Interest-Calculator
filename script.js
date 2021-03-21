d7edc72c831a86396906bb067134318c9cb59695
ktyptorio <caesarioriorio.kisty@gmail.com>
2021-03-21 06:39:54 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    principal = document.getElementById("principal").value;
+    if (principal<=0){
+        document.getElementById("principal").focus();
+        alert("Principal must be greater than 0");
+        return false;
+    }
+        if (principal==""){
+        document.getElementById("principal").focus();
+        alert("Principal must be greater than 0");
+        return false;
+    }
+
+    principal = document.getElementById("principal").value;
+    rate=document.getElementById("rate").value;
+    years=document.getElementById("years").value;
+    result=(principal*rate/100)*years;
+    year=2020+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit "+principal+","+"<br>"+
+    "at interest of "+rate+"."+"<br>"+
+    "You will receive an amount of "+result+","+"<br>"+
+    "in the year "+year;
+
+
 }
-        
+    
+function showrate(){
+    document.getElementById("id_rate").innerHTML=document.getElementById("rate").value;
+}  
